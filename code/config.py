323adb9a4b4c88fd3b920e444a0edf927971a5e3
Carlo Leone <carlo.fanton92@gmail.com>
2020-06-16 19:51:28 +0200

    1880 - 1945 maps

--- code/config.py
+++ code/config.py
@@ -1,1 +1,7 @@
 EARTH_RADIUS = 6371
+
+GEOJSON_1880 = '../data/1880.geojson'
+GEOJSON_1914 = '../data/1914.geojson'
+GEOJSON_1920 = '../data/1920.geojson'
+GEOJSON_1938 = '../data/1938.geojson'
+GEOJSON_1945 = '../data/1945.geojson'
--- code/mapPlotter.py
+++ code/mapPlotter.py
@@ -1,65 +1,62 @@
-import math
+import pandas as pd
+import numpy as np
+import geopandas
 import folium
+from folium.plugins import TimeSliderChoropleth
 import matplotlib
+import datetime
+import colorsys
 import config
 
-R = config.EARTH_RADIUS
+geojsonData_1880 = geopandas.read_file(config.GEOJSON_1880)
+geojsonData_1914 = geopandas.read_file(config.GEOJSON_1914)
+geojsonData_1920 = geopandas.read_file(config.GEOJSON_1920)
+geojsonData_1938 = geopandas.read_file(config.GEOJSON_1938)
+geojsonData_1945 = geopandas.read_file(config.GEOJSON_1945)
+
+years = {'Date': ['01-01-1880', '01-01-1914', '01-01-1920', '01-01-1938', '01-01-1945']}
+df = pd.DataFrame(years)
+df['Date'] = pd.to_datetime(df['Date'])
+# print(df)
+datetime_index = pd.DatetimeIndex(df['Date'])
+dt_index_epochs = datetime_index.astype(int) // 10**9
+dt_index = dt_index_epochs.astype('U10')
+# print(dt_index)
 
 
-def to_radiants(degrees):
-    radiants = degrees * math.pi / 180
-    return radiants
-
-
-def to_degrees(radiants):
-    degrees = radiants * 180 / math.pi
-    return degrees
-
-
-def waypoint(phi_1, gamma_1, theta, d):
-    phi_2 = math.asin(math.sin(phi_1) * math.cos(d/R) + math.cos(phi_1) * math.sin(d/R) * math.cos(theta))
-    gamma_2 = gamma_1 + math.atan2(math.sin(theta) * math.sin(d/R) * math.cos(phi_1),
-                                   math.cos(d/R) - math.sin(phi_1) * math.sin(phi_2))
-    # normalize between [-180°, +180°]
-    gamma_2 = (gamma_2 + 3 * math.pi) % (2 * math.pi) - math.pi
-    return phi_2, gamma_2
+def create_rgb_colors(color_number):
+    HSV_tuples = [(x * 1.0 / color_number, 0.5, 0.5) for x in range(color_number)]
+    RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+    return list(RGB_tuples)
 
 
 if __name__ == "__main__":
-    # Valparaiso
-    phi_v = to_radiants(41.47)
-    gamma_v = to_radiants(-87)
-    # Shangai
-    phi_s = to_radiants(31.4)
-    gamma_s = to_radiants(121.8)
-    # Compute d and theta
-    d = R * math.acos(math.sin(phi_v) * math.sin(phi_s) + math.cos(phi_v) * math.cos(phi_s) * math.cos(gamma_s - gamma_v))
-    theta = math.atan2(math.sin(gamma_s - gamma_v) * math.cos(phi_s),
-                       math.cos(phi_v) * math.sin(phi_s) - math.sin(phi_v) * math.cos(phi_s) * math.cos(gamma_s - gamma_v))
+    # 'NAME', 'geometry'
+    a = geojsonData_1945.head(10)
+    colors = create_rgb_colors(5)
 
-    #a = waypoint(phi_v, phi_s, theta, d)
-    markers = []
-    waypoints = []
-    d_step = 1000
-    for i in range(0, math.floor(d), d_step):
-        w = waypoint(phi_v, phi_s, theta, i)
-        waypoints.append(w)
+    # darkseagreen, green, red, yellow, pink
+    styles = [{'fillColor': '#FF69B4', 'color': '#FF69B4'},
+              {'fillColor': '#228B22', 'color': '#228B22'},
+              {'fillColor': '#FF3030', 'color': '#FF3030'},
+              {'fillColor': '#EEC900', 'color': '#EEC900'},
+              {'fillColor': '#C1FFC1', 'color': '#C1FFC1'}]
+    """
+    styles = []
+    for c in colors:
+        hex_color = '#%02x%02x%02x' % c
+        style = {'fillColor': str(hex_color), 'color:': str(hex_color)}
+        styles.append(style)
+    """
 
-    for w in waypoints:
-        lat = to_degrees(w[0])
-        long = to_degrees((w[1]))
-        marker = [lat, long]
-        markers.append(marker)
+    now = datetime.datetime.now()
+    m = folium.Map([41, 12], tiles='cartodbpositron', zoom_start=3)
 
-    # markers.append([31.4, 121.8])
+    folium.GeoJson(geojsonData_1880, name='1880', style_function=lambda x: styles[0], show=False).add_to(m)
+    folium.GeoJson(geojsonData_1914, name='1914', style_function=lambda x: styles[1], show=False).add_to(m)
+    folium.GeoJson(geojsonData_1920, name='1920', style_function=lambda x: styles[2], show=False).add_to(m)
+    folium.GeoJson(geojsonData_1938, name='1938', style_function=lambda x: styles[3], show=False).add_to(m)
+    folium.GeoJson(geojsonData_1945, name='1945', style_function=lambda x: styles[4], show=False).add_to(m)
+    folium.LayerControl().add_to(m)
 
-    fig5 = folium.Figure(height=550, width=750)
-    m5 = folium.Map(location=markers[0], tiles='cartodbpositron', zoom_start=5)
-    fig5.add_child(m5)
-
-    line_1 = folium.vector_layers.PolyLine(markers, popup='<b>Path</b>', tooltip='Vehicle', color='blue', weight=5).add_to(m5)
-    folium.LayerControl().add_to(m5)
-
-    m5.save('test.html')
-
-    print("Done")
+    m.save('morphoWorld.html')
