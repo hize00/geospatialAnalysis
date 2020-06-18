fcebd9b3084ee06b4a321156d97653b7c77b415c
Carlo Leone <carlo.fanton92@gmail.com>
2020-06-18 19:44:33 +0200

    layers color fixed

--- code/mapPlotter.py
+++ code/mapPlotter.py
@@ -1,11 +1,7 @@
-import pandas as pd
-import numpy as np
 import geopandas
 import folium
-from folium.plugins import TimeSliderChoropleth, HeatMapWithTime
-import matplotlib
-import datetime
-import colorsys
+import sys
+import os
 import config
 
 RESULT_FOLDER = config.RESULT_FOLDER
@@ -29,8 +25,8 @@ geojsonData_1938 = geopandas.read_file(config.GEOJSON_1938)
 geojsonData_1945 = geopandas.read_file(config.GEOJSON_1945)
 geojsonData_1994 = geopandas.read_file(config.GEOJSON_1994)
 
-GEOJSONS = {'-323': geojsonData_323bc,
-            '-200': geojsonData_200bc,
+GEOJSONS = {'323BC': geojsonData_323bc,
+            '200BC': geojsonData_200bc,
             '0': geojsonData_1bc,
             '400': geojsonData_400,
             '800': geojsonData_800,
@@ -47,61 +43,51 @@ GEOJSONS = {'-323': geojsonData_323bc,
             '1945': geojsonData_1945,
             '1994': geojsonData_1994}
 
-GEOJSONS_SORTED = sorted(GEOJSONS, key=lambda x: int(x))
+GEOJSONS_SORTED = sorted(GEOJSONS, key=lambda x: int(x) if 'BC' not in x else int(x.replace('BC', ''))*-1)
+
+
+def simple_rounding(x):
+    i, f = divmod(x, 1)
+    return int(i + ((f >= 0.5) if (x > 0) else (f > 0.5)))
 
 
 if __name__ == "__main__":
     try:
-        # 'NAME', 'geometry'
-        a = geojsonData_1945.head(10)
         # Palettes: blue - red - green
         colors = ['#80ffdb', '#72efdd', '#64dfdf', '#56cfe1', '#48bfe3', '#4ea8de', '#5390d9', '#5e60ce', '#6930c3', '#7400b8',
-                  #'#ffba08', '#faa307', '#f48c06', '#e85d04', '#dc2f02', '#d00000', '#9d0208', '#6a040f',
-                  '#ffba08', '#faa307', '#f48c06', '#e85d04', '#dc2f02', '#d00000', 'pink', '#6a040f',
-                  '#ffff3f', '#eeef20', '#dddf00', '#d4d700', '#bfd200', '#aacc00', '#80b918', '#55a630', '#2b9348', '#007f5f']
+                  '#ffff3f', '#eeef20', '#ffba08', '#faa307', '#f48c06', '#e85d04', '#dc2f02', '#d00000', '#9d0208', '#6a040f',
+                   '#dddf00', '#d4d700', '#bfd200', '#aacc00', '#80b918', '#55a630', '#2b9348', '#007f5f']
         styles = []
         for c in colors:
             style = {'fillColor': c, 'color': c}
             styles.append(style)
 
+        delta_idx_color = 1
+        if len(GEOJSONS) < len(colors):
+            delta_idx_color = len(colors) / len(GEOJSONS)
+        else:
+            print("WARNING: you are trying to plot a higher number of maps with respect to the number of colors provided. Some colors will be repeated")
+
         m = folium.Map([41, 12], tiles='cartodbpositron', zoom_start=3)
 
         for idx, key in enumerate(GEOJSONS_SORTED):
-            folium.GeoJson(GEOJSONS[key], name=key, style_function=lambda x: styles[idx], show=False,
-                           tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-            print('Added map ' + key + ' with color ' + colors[idx])
-
+            color_idx = simple_rounding(delta_idx_color * idx)
+            if color_idx >= len(GEOJSONS):
+                color_idx = idx
+            styleColor = styles[color_idx]
+            # careful about late binding
+            layer = folium.GeoJson(GEOJSONS[key], name=key, style_function=lambda x, color=styleColor: color, show=False,
+                                   tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY']))
+            layer.add_to(m)
+            print('Added layer ' + key + ' with color ' + colors[color_idx])
         folium.LayerControl(collapsed=False).add_to(m)
 
         output_path = RESULT_FOLDER + '/morphoWorld.html'
         m.save(output_path)
 
-        """   
-        styles = [{'fillColor': '#FFBA08', 'color': '#FFBA08'},
-                  {'fillColor': '#FAA307', 'color': '#FAA307'},
-                  {'fillColor': '#F48C06', 'color': '#F48C06'},
-                  {'fillColor': '#E85D04', 'color': '#E85D04'},
-                  {'fillColor': '#DC2F02', 'color': '#DC2F02'},
-                  {'fillColor': '#D00000', 'color': '#D00000'},
-                  {'fillColor': '#9D0208', 'color': '#9D0208'},
-                  {'fillColor': '#6A040F', 'color': '#6A040F'},
-                  {'fillColor': '#370617', 'color': '#370617'},
-                  {'fillColor': '#03071E', 'color': '#03071E'}
-                  ]
-    
-        folium.GeoJson(geojsonData_1880, name='1880', style_function=lambda x: styles[0], show=False,
-                       tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-        folium.GeoJson(geojsonData_1914, name='1914', style_function=lambda x: styles[1], show=False,
-                       tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-        folium.GeoJson(geojsonData_1920, name='1920', style_function=lambda x: styles[2], show=False,
-                       tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-        folium.GeoJson(geojsonData_1938, name='1938', style_function=lambda x: styles[3], show=False,
-                       tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-        folium.GeoJson(geojsonData_1945, name='1945', style_function=lambda x: styles[4], show=False,
-                       tooltip=folium.features.GeoJsonTooltip(fields=['NAME'], aliases=['COUNTRY'])).add_to(m)
-        """
     except Exception as e:
-        print("\n---Exception occurred---\n")
-        print(e)
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
 
 
